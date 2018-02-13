9154aed3424a7fce5d3b3995bd7c2f452fdf0353
Romain Dardour <romain@hull.io>
2018-02-13 15:59:55 +0100

    fix batchHandler

--- server/handlers/batch.js
+++ server/handlers/batch.js
@@ -1,28 +1,23 @@
 /* @flow */
 
-import { batcherHandler } from "hull/lib/utils";
+import { notifHandler } from "hull/lib/utils";
 import updateUser from "../lib/update-user";
 
-const batch = batcherHandler(
-  ({ metric, client, ship }: Object, messages: Array<Object> = []) => {
-    client.logger.debug("outgoing.batch.process", {
-      messages: messages.length
-    });
-    messages.map(m =>
-      updateUser(
-        {
-          metric,
-          client,
-          ship,
-          isBatch: true
-        },
-        m
-      )
-    );
-  },
+const batch = notifHandler(
   {
     groupTraits: true,
     batchSize: 100
+  },
+  {
+    "user:update": (
+      { metric, client, ship }: Object,
+      messages: Array<Object> = []
+    ) => {
+      client.logger.debug("outgoing.batch.process", {
+        messages: messages.length
+      });
+      messages.map(m => updateUser({ metric, client, ship, isBatch: true }, m));
+    }
   }
 );
 
