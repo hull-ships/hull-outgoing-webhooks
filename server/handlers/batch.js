ea541ac2552dd5f0b9b60cc6a913026889297666
Romain Dardour <romain@hull.io>
2018-02-13 16:05:30 +0100

    fix for real

--- server/handlers/batch.js
+++ server/handlers/batch.js
@@ -3,12 +3,12 @@
 import { notifHandler } from "hull/lib/utils";
 import updateUser from "../lib/update-user";
 
-const batch = notifHandler(
-  {
+const batch = notifHandler({
+  userHandlerOptions: {
     groupTraits: true,
     batchSize: 100
   },
-  {
+  handlers: {
     "user:update": (
       { metric, client, ship }: Object,
       messages: Array<Object> = []
@@ -19,6 +19,6 @@ const batch = notifHandler(
       messages.map(m => updateUser({ metric, client, ship, isBatch: true }, m));
     }
   }
-);
+});
 
 export default batch;
