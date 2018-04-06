8d707829b11d8e1f1df7d43712e9ddc7fab4d5fe
Michal Raczka <michal@raczka.me>
2018-04-06 12:38:28 +0200

    Fix

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 const { batchHandler, statusHandler, notifyHandler } = require("./handlers");
 
-function server(options = {}) {
+function server(app, options = {}) {
   app.post("/smart-notifier", notifyHandler);
   app.use("/batch", batchHandler);
   app.all("/status", statusHandler);
