2acaa0fb9302d9f6ba5f18a28e3772f803968007
Andy Hull <andy@hull.io>
2019-05-16 11:01:04 -0400

    event filtering'

--- server/util/get-entity-matched-events.js
+++ server/util/get-entity-matched-events.js
@@ -1,24 +1,10 @@
 const _ = require("lodash");
 
 const getEntityMatchedEvents = (triggeredEvents, notifyEvents) => {
-  let event_hash = [];
-
-  if (notifyEvents.length) {
-    const event_names = _.map(triggeredEvents, "event");
-
-    event_hash = _.compact(
-      _.uniq(
-        _.forEach(notifyEvents, event => {
-          if (_.includes(event_names, event)) {
-            return event;
-          }
-          return undefined;
-        })
-      )
-    );
-  }
-
-  return event_hash;
+  const matchedEvents = _.filter(triggeredEvents, event =>
+    _.includes(notifyEvents, event.event)
+  );
+  return matchedEvents;
 };
 
 module.exports = getEntityMatchedEvents;
