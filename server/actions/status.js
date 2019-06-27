161b40ef851817dd694e06f85ffd7f500259aa02
Andy Hull <andy@hull.io>
2019-06-27 13:26:11 -0400

    fix status

--- server/actions/status.js
+++ server/actions/status.js
@@ -7,71 +7,26 @@ function statusCheck(req, res) {
     webhooks_urls,
     webhooks_account_urls,
     synchronized_segments,
-    synchronized_account_segments,
-    webhooks_attributes,
-    webhooks_account_attributes,
-    webhooks_events,
-    webhooks_account_events,
-    webhooks_anytime,
-    webhooks_account_anytime,
-    webhooks_segments,
-    webhooks_account_segments
+    synchronized_account_segments
   } = private_settings;
 
   let status = "ok";
   const messages = [];
 
-  if (!_.size(webhooks_urls)) {
-    status = "warning";
-    messages.push(
-      "No user webhooks configured. Connector won't send anything for the user."
-    );
-  }
-
-  if (!_.size(webhooks_account_urls)) {
-    status = "warning";
-    messages.push(
-      "No account webhooks configured. Connector won't send anything for the account."
-    );
-  }
-
-  if (!_.size(synchronized_segments)) {
-    status = "warning";
-    messages.push(
-      "No user segments are listed. Connector won't send anything for the user."
-    );
-  }
-
-  if (!_.size(synchronized_account_segments)) {
-    status = "warning";
-    messages.push(
-      "No account segments are listed. Connector won't send anything for the account."
-    );
-  }
-
   if (
-    !webhooks_anytime &&
-    !_.size(webhooks_segments) &&
-    !_.some(_.map(webhooks_segments, v => v.entered || v.left)) &&
-    !_.size(webhooks_events) &&
-    !_.size(webhooks_attributes)
+    !_.size(synchronized_segments) &&
+    !_.size(synchronized_account_segments)
   ) {
     status = "warning";
     messages.push(
-      "No user output condition active. Connector won't send anything for the user."
+      "No user or account segments are listed. Connector won't send any payload."
     );
   }
 
-  if (
-    !webhooks_account_anytime &&
-    !_.size(webhooks_account_segments) &&
-    !_.some(_.map(webhooks_account_segments, v => v.entered || v.left)) &&
-    !_.size(webhooks_account_events) &&
-    !_.size(webhooks_account_attributes)
-  ) {
+  if (!_.size(webhooks_urls) && !_.size(webhooks_account_urls)) {
     status = "warning";
     messages.push(
-      "No account output condition active. Connector won't send anything for the account."
+      "No user webhooks configured. Connector won't send any payload."
     );
   }
 
